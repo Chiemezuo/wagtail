74f0f18ae8185afaa500d7aefbc99e9629ec261c
Chiemezuo <chiemezuoakujobi@gmail.com>
2025-06-11 01:33:13 +0100

    fix w-formset rendering issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,7 +41,6 @@ module.exports = function exports(env, argv) {
       'draftail',
       'filtered-select',
       'icons',
-      'inline-panel',
       'modal-workflow',
       'page-chooser-modal',
       'page-chooser',
